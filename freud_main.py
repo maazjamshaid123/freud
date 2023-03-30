b24e7abf9170805ed20501e28d3b020233e23abc
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2023-03-30 15:50:48 +0500

    Add files via upload

--- freud_main.py
+++ freud_main.py
@@ -1,7 +1,12 @@
 import streamlit as st
+import pyttsx3
 from freud import CustomChatGPT
 
-st.set_page_config(page_title = 'Freud',layout="wide",page_icon="Freud.png")
+st.set_page_config(page_title='Freud', layout="wide", page_icon="Freud.png", menu_items={
+        'Get Help': 'https://www.linkedin.com/in/maazjamshaid/',
+        'Report a bug': "https://www.linkedin.com/in/maazjamshaid/",
+        'About': '''$Freud$ by [Maaz Jamshaid](https://www.linkedin.com/in/maazjamshaid/), maaz@astroalgo.com'''
+    })
 
 st.markdown("---")
 
@@ -15,4 +20,10 @@ user_input = st.text_input("Give me a detailed description about your dream: ")
 
 if st.button("Ask Freud"):
     response = CustomChatGPT(user_input)
-    st.text_area("Patient:", value=response, height=200, max_chars=None, key=None)
+    st.text_area("Freud:", value=response, height=200, max_chars=None, key=None)
+
+    # Initialize pyttsx3 TTS engine
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  # Adjust the speech rate (default is 200)
+    engine.say(response)
+    engine.runAndWait()
