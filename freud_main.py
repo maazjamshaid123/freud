7e45e75390501bd1fc00bc309709e5a13ec4faf9
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2023-03-30 16:06:26 +0500

    Add files via upload

--- freud_main.py
+++ freud_main.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import pyttsx3
-import os
 from freud import CustomChatGPT
 
-st.set_page_config(page_title='Freud', layout="wide", page_icon="Freud.png", menu_items={
+st.set_page_config(page_title = 'Freud',layout="wide",page_icon="Freud.png", menu_items={
         'Get Help': 'https://www.linkedin.com/in/maazjamshaid/',
         'Report a bug': "https://www.linkedin.com/in/maazjamshaid/",
         'About': '''$Freud$ by [Maaz Jamshaid](https://www.linkedin.com/in/maazjamshaid/), maaz@astroalgo.com'''
@@ -22,13 +20,3 @@ user_input = st.text_input("Give me a detailed description about your dream: ")
 if st.button("Ask Freud"):
     response = CustomChatGPT(user_input)
     st.text_area("Patient:", value=response, height=200, max_chars=None, key=None)
-
-    # Set the LD_LIBRARY_PATH variable to the directory containing the libespeak.so.1 file
-    espeak_path = "/usr/lib/x86_64-linux-gnu"
-    os.environ["LD_LIBRARY_PATH"] = espeak_path
-
-    # Initialize pyttsx3 TTS engine
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)  # Adjust the speech rate (default is 200)
-    engine.say(response)
-    engine.runAndWait()
