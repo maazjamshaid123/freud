69c77051a6634d7eb588a56c8f87fb6cf229fc2c
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2023-03-30 22:19:04 +0500

    Update freud_main.py

--- freud_main.py
+++ freud_main.py
@@ -20,4 +20,4 @@ user_input = st.text_input("Give me a detailed description about your dream: ")
 
 if st.button("Ask Freud"):
     response = CustomChatGPT(user_input)
-    st.text_area("Patient:", value=response, height=100, max_chars=None, key=None)
+    st.text_area("Freud:", value=response, height=100, max_chars=None, key=None)
